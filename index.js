0b54e95790dd708601108a91130168d8e901d6e7
Blaxat <akshaypandey047@gmail.com>
2023-07-29 19:53:05 +0530

    commit

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-require('dotenv').config()
+const express = require("express");
+const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
-const PORT = process.env.PORT;
+const app = express();
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
 console.log(PORT);
 
-const io = new Server(PORT, {
-  cors: true,
-});
+app.use(cors());
+
+const io = new Server(server);
 
 const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
 
 io.on("connection", (socket) => {
-  console.log(`Socket Connected`, socket.id);
   socket.on("room:join", (data) => {
     const { email, room } = data;
     emailToSocketIdMap.set(email, socket.id);
@@ -32,12 +36,14 @@ io.on("connection", (socket) => {
   });
 
   socket.on("peer:nego:needed", ({ to, offer }) => {
-    console.log("peer:nego:needed", offer);
     io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
   });
 
   socket.on("peer:nego:done", ({ to, ans }) => {
-    console.log("peer:nego:done", ans);
     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
   });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
